6e11e9b253e403f975d0c16eb1f9f3f73dd4d254
Manoj Bhatt <freelancingcode20@gmail.com>
2022-09-29 22:01:02 +0530

    Updates

---
+++ precache-manifest.525a0f8d7996041f4d5ab5aee2e234e6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c28626f2a93d9eff09a2c10b627a7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50f1278036fc49d91305",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f5373e189e9d043ad3c6",
+    "url": "/static/css/main.d92d1bdb.chunk.css"
+  },
+  {
+    "revision": "50f1278036fc49d91305",
+    "url": "/static/js/2.699b04c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.699b04c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5373e189e9d043ad3c6",
+    "url": "/static/js/main.d77f3d28.chunk.js"
+  },
+  {
+    "revision": "b4a1231fcf43a6cfa08b",
+    "url": "/static/js/runtime-main.f6863a35.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "cecd05a1ed86babfb5708a4bc11d0776",
+    "url": "/static/media/veg.cecd05a1.png"
+  }
+]);
